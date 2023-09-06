9a2e4dd26d1c7afa32dc8bfab47402c4e23cd0f8
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-09-06 15:05:19 +0200

    complete mileston 2 and bonus 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,34 +9,34 @@ Creare l’array di oggetti con le informazioni fornite.
 
 const ourTeam = [
     {
-        name : 'Wayne Barnett',
-        position : 'Founder & CEO',
-        profileImg : "./assets/img/wayne-barnett-founder-ceo.jpg"
+        name: 'Wayne Barnett',
+        position: 'Founder & CEO',
+        profileImg: "./assets/img/wayne-barnett-founder-ceo.jpg"
     },
     {
-        name : 'Angela Caroll',
-        position : 'Chief Editor',
-        profileImg : "./assets/img/angela-caroll-chief-editor.jpg"
+        name: 'Angela Caroll',
+        position: 'Chief Editor',
+        profileImg: "./assets/img/angela-caroll-chief-editor.jpg"
     },
     {
-        name : 'Walter Gordon',
-        position : 'Office Manager',
-        profileImg : "./assets/img/walter-gordon-office-manager.jpg"
+        name: 'Walter Gordon',
+        position: 'Office Manager',
+        profileImg: "./assets/img/walter-gordon-office-manager.jpg"
     },
     {
-        name : 'Angela Lopez',
-        position : 'Social Media Manager',
-        profileImg : "./assets/img/angela-lopez-social-media-manager.jpg"
+        name: 'Angela Lopez',
+        position: 'Social Media Manager',
+        profileImg: "./assets/img/angela-lopez-social-media-manager.jpg"
     },
     {
-        name : 'Scott Estrada',
-        position : 'Developer',
-        profileImg : "./assets/img/scott-estrada-developer.jpg"
+        name: 'Scott Estrada',
+        position: 'Developer',
+        profileImg: "./assets/img/scott-estrada-developer.jpg"
     },
     {
-        name : 'Barbara Ramos',
-        position : 'Graphic Designer',
-        profileImg : "./assets/img/barbara-ramos-graphic-designer.jpg"
+        name: 'Barbara Ramos',
+        position: 'Graphic Designer',
+        profileImg: "./assets/img/barbara-ramos-graphic-designer.jpg"
     },
 
 ]
@@ -46,14 +46,44 @@ Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e 
 
 
 for (let i = 0; i < ourTeam.length; i++) {
-    
+
     const teamComponent = ourTeam[i];
 
     //console.log(teamComponent);
 
     for (const key in teamComponent) {
 
-        console.log(key,teamComponent[key]);
+        console.log(key, teamComponent[key]);
     }
-    
+
+}
+
+/* MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+ */
+
+//use the same cicle for that i used before, instead of console.log i use insertAdiacentHTML
+
+//dove inserisco =>
+const teamComponentDOM = document.querySelector('.row');
+
+for (let i = 0; i < ourTeam.length; i++) {
+
+    const teamComponent = ourTeam[i];
+
+    const teamComponentMarkup = `<div>
+    <h4>Nome: ${teamComponent.name}</h4>
+    <h3>Ruolo: ${teamComponent.position}</h3>
+    <img src="${teamComponent.profileImg}" alt="">
+</div>`
+
+    teamComponentDOM.insertAdjacentHTML('beforeend', teamComponentMarkup);
+
+    //console.log(teamComponent);
+
+    for (const key in teamComponent) {
+
+        console.log(key, teamComponent[key]);
+    }
+
 }
